ce273d4a9b44c3a13ea810a382238cf4ba4ecefe
synka777 <myega@rigdar.com>
2026-04-08 13:25:43 +0200

    Fix: ingestion de données

--- config/config.py
+++ config/config.py
@@ -13,11 +13,11 @@ DATA_RAW_PATH = PROJECT_ROOT / "data" / "raw"
 DATA_PROCESSED_PATH = PROJECT_ROOT / "data" / "processed"
 
 # PostgreSQL
-DB_HOST = os.getenv("DB_HOST", "postgres")
-DB_PORT = int(os.getenv("DB_PORT", 5432))
-DB_NAME = os.getenv("DB_NAME", "rfm_db")
-DB_USER = os.getenv("DB_USER", "rfm_user")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "rfm_password")
+DB_HOST = "localhost"
+DB_PORT = 5432
+DB_NAME = "rfm_db"
+DB_USER = "rfm_user"
+DB_PASSWORD = "rfm_password"
 
 # URLs
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
